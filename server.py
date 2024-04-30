90b3efd9b0cb37faca8f17531cc5c8186a5befc8
Karsten <karsten.griffo@gmail.com>
2024-04-30 18:41:52 -0400

    front end setup

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI()
+app.mount("/", StaticFiles(directory="ui/dist", html=True), name="ui")
 
 
 @app.get("/api/v1/hello")
